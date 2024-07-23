e273f8a4bc5aa07c7977c3e9229925cf71957aba
Victor-PC <filpsixi@mail.ru>
2024-07-23 07:43:36 +0300

    front

--- meoteo_proj/main_app/static/js/api.js
+++ meoteo_proj/main_app/static/js/api.js
@@ -1,10 +1,15 @@
 " Здесь хранятся функции для создания и обработки запросов "
 " При вводе города  искать город в истории (базе данных)"
 " Если город не найден в истории, искать его в 2gis "
-" После полного ввода его названия получить географические координты (из истории или с 2gis) "
+" После полного ввода его названия получить географические координаты (из истории или с 2gis) "
 "После получения координат делается запрос на https://open-meteo.com/"
-search_city_in_our_api = () = {
-    let request = new XMLHttpRequest();
-    request.open("GET", "")
+import {NON_SUPPORT_MODE} from "/static/js/conf.js";
+
+
+var search_city_in_2gis_api = () => {};
+var get_current_timezone_offset = () => {
+    let offset = new Date().getTimezoneOffset()
+    let o = Math.abs(offset);
+    return (offset < 0 ? "+" : "-") + ("00" + Math.floor(o / 60)).slice(-2) + ":" + ("00" + (o % 60)).slice(-2);
 };
-search_city_in_2gis_api = () {};
+console.log(get_current_timezone_offset())
--- meoteo_proj/main_app/static/js/form.js
+++ meoteo_proj/main_app/static/js/form.js
@@ -1,6 +1,55 @@
-https://catalog.api.2gis.com/2.0/region/search?q=Мо&fields=items.point&key=3a5c3351-ed98-4d29-92b0-30c8d01ad7aa  № ищем города
+"Форма реагирует на событие change выполняя запрос на api - Живой поиск"
+"Форма по событию submit делает запрос на получение географических координат"
+" https://catalog.api.2gis.com/2.0/region/search?q=Мо&fields=items.point&key=3a5c3351-ed98-4d29-92b0-30c8d01ad7aa   ищем города"
+" https://catalog.api.2gis.com/3.0/items?q=Москва&type=adm_div.city&key=3a5c3351-ed98-4d29-92b0-30c8d01ad7aa&fields=items.point получаем координаты "
+const RATE = 2; // Вызов на api не чаще 1 раза в n секунд
 
-https://catalog.api.2gis.com/3.0/items?q=Москва&type=adm_div.city&key=3a5c3351-ed98-4d29-92b0-30c8d01ad7aa&fields=items.point
-# координаты города
+// locker
+var lock = false;
+var init_locker = () => {
+    var lock = true;
+    setTimeOut(() => {
+        var lock = false;
+    }, RATE);
+};
+// end locker
+var get_cities_input_value = () => {
+    return document.forms[0].getElementsByClassName("form-control")[0].value;
+};
+var create_request_body = (type, item) => {
+    return JSON.stringify({
+        "type": type,
+        "item": item
+    });
+};
+var push_new_task_in_queue = (id) => {
+    queue.push(id);
+};
+window.onload = () => {
+    var search_city_names = (e) => {
+        var request = new XMLHttpRequest();
+        request.open("POST",  get_search_city_api_url(), true);
+        request.onload = (event) => {
+            if (!event.readyState == 4) {
+                return
+            }
+            if (!event.target.status == 200) {
+                return
+            }
+            let task_id = event.target.data["task_id"]
+            push_new_task_in_queue(task_id);
+        };
+        request.setRequestHeader("X-CSRFToken", get_token());
+        request.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+        request.setRequestHeader("content-type", "application/json");
+        let text = get_cities_input_value();
+        if (!text) {
+            return
+        }
+        request.send(create_request_body("search_city", text));
+    };
+    var replace_choices_list = () => {
 
-
+    };
+    document.forms[0].getElementsByClassName("form-control")[0].addEventListener("change", search_city_names);
+};
